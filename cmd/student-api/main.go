49ee4420416308285c940fdfa593261b7105bd92
Bikesh <bigupta@lenovo.com>
2024-09-29 21:06:55 +0530

    Added config logic

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Dev25-os/student-api/internal/config"
+)
 
 func main() {
 	fmt.Println("Welcome to student api")
+	//load config
+	cfg := config.MustLoad()
+	fmt.Print("Config file", cfg)
+	// database setup
+	//setup router
+	//setup server
 
 }
